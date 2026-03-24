17ccaa5f33b10ad627a1d3190bcd99a7782d9da2
PrajaktaBapat09 <prajaktabapat032@gmail.com>
2026-03-24 19:09:12 +0530

    Problem 1 solved

--- arrays/min_max.cpp
+++ arrays/min_max.cpp
@@ -1,1 +1,22 @@
-
+//Problem 1: Find the minimum element
+#include <limits.h>
+using namespace std;
+//Find minimum element of the array
+int main()
+{
+    int array [] = {12,5,8,20,3,15};
+    int size = 6;
+    int i;
+    int min = INT_MAX;
+    for(i=0;i<size;i++)
+    {
+        if(array[i]<min)
+        {
+            min = array[i];
+        }
+    }
+    cout<<"Minimum Element: "<<min;
+    return 0;
+}
+//Expected Output: 3
+//My Output: 3
