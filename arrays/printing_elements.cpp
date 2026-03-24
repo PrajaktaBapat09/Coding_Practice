949f99f8da6bf3a5d50e8a96af5f032aabc425f4
PrajaktaBapat09 <prajaktabapat032@gmail.com>
2026-03-24 18:47:12 +0530

    Problem 2 solved

--- arrays/printing_elements.cpp
+++ arrays/printing_elements.cpp
@@ -22,3 +22,22 @@ int main()
 
 //Expected Output : 10 30 50
 //My Output : 10 30 50
+
+
+// Problem 2: Print elements of array in reverse order
+#include <iostream>
+using namespace std;
+//Print elements of array at the reverse order
+int main()
+{
+    int array [] = {5,15,25,35,45};
+    int i; 
+    int size = 5;
+    for(i= size-1; i>=0; i--)
+    {
+        cout<<array[i]<<" ";
+    }
+    return 0;
+}
+//Expected Output: 45 35 25 15 5
+//My Output: 45 35 25 15 5
