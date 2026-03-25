0b58d330a91875263eeff0d1939851f0c680e504
PrajaktaBapat09 <prajaktabapat032@gmail.com>
2026-03-25 09:54:26 +0530

    Problem 3 solved

--- arrays/min_max.cpp
+++ arrays/min_max.cpp
@@ -46,3 +46,35 @@ int main()
 
 //Expected Output: 20
 //My Output: 20
+
+//Problem 3: Finding difference between the max and min element of the given array
+#include <iostream>
+#include <limits.h>
+using namespace std;
+//Finding the difference between min and max elements of the array
+int main()
+{
+    int array[] = {7,2,9,4,1};
+    int i;
+    int size = 5;
+    int min = INT_MAX;
+    int max = INT_MIN;
+    for(i=0;i<size;i++)
+    {
+        if(array[i]>max)
+        {
+            max = array[i];
+        }
+        if(array[i]<min)
+        {
+            min = array[i];
+        }
+    }
+    cout<<max<<endl;
+    cout<<min<<endl;
+    cout<<max-min<<endl;
+    return 0;
+}
+
+//Expected Output: 9 8 1
+//My Output: 9 8 1
