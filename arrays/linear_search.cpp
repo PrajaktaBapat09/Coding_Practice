6ba539c4429b122d9f8fe56c4154f45f6a69f5c1
PrajaktaBapat09 <prajaktabapat032@gmail.com>
2026-03-25 11:17:06 +0530

    Problem 2 solved

--- arrays/linear_search.cpp
+++ arrays/linear_search.cpp
@@ -20,3 +20,29 @@ int main()
 
 //Expected Output: Found
 //My Output: Found
+
+
+//Problem 2: To count the number of occurences of the target element in the given array
+#include <iostream>
+using namespace std;
+//To count the number of occurences of the target element in the given array
+int main()
+{
+    int array[]={1,2,2,3,2,4};
+    int i;
+    int size=6;
+    int target=2;
+    int count = 0;
+    for(i=0;i<size;i++)
+    {
+        if(array[i]==target)
+        {
+            count = count + 1;
+        }
+    }
+    cout<<count;
+    return 0;
+}
+
+//Expected Output: 3
+//My Output: 3
